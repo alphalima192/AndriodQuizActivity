39a9ac741bbcdb3139c0c41fa8b623dce258d4d3
acooper5020 <firefighter-1154@hotmail.com>
2018-02-26 12:13:12 -0500

    Play with life cycle

--- app/src/main/java/edu/brcc/maxfieldj/quizactivity/QuizActivity.java
+++ app/src/main/java/edu/brcc/maxfieldj/quizactivity/QuizActivity.java
@@ -2,6 +2,7 @@ package edu.brcc.maxfieldj.quizactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +10,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+    private static final String TAG = "QuizActivity"; //appears in log down below
+    static final String CURRENT_INDEX = "currentIndex";
+
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -25,12 +29,22 @@ public class QuizActivity extends AppCompatActivity {
             new Question(R.string.question_mideast, false)
     };
 
-    private int mCurrentIndex = 0;
+    private int mCurrentIndex;//declared below
 
     @Override
+    // See https://developer.android.com/guide/components/activities/activity-lifecycle.html for
+    // the callbacks and instance states
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Check whether we're recreating a previously destroyed instance
+        if (savedInstanceState != null) {
+            // Restore value of members from saved state
+            mCurrentIndex = savedInstanceState.getInt(CURRENT_INDEX);
+        } else {
+            mCurrentIndex = 0;
+        }
         setContentView(R.layout.activity_quiz);
+        Log.d(TAG, "onCreate");
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
 //        int question = mQuestionBank[mCurrentIndex].getTextResId();
@@ -111,5 +125,44 @@ public class QuizActivity extends AppCompatActivity {
         Toast toast = Toast.makeText(this,messageResId,Toast.LENGTH_SHORT);
         toast.setGravity(Gravity.TOP,0,250);
         toast.show();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart"); //log.d writes debug messages
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(TAG,"onStop");
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+    @Override
+    public void onRestart(){
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    public void onSaveInstanceState(Bundle savedInstanceState) { //save the state of the app
+        // Save the user's current game state
+        savedInstanceState.putInt(CURRENT_INDEX, mCurrentIndex);
+        Log.d(TAG, "onSaveInstanceState");
+
+        // Always call the superclass so it can save the view hierarchy state
+        super.onSaveInstanceState(savedInstanceState);
     }
 }
